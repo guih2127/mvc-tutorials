a20ac6addb9385a8b5655633a7ba8d8c90b4a207
guih2127 <guih2127@gmail.com>
2019-01-14 01:39:03 -0200

    first model, migration and crud (scaffolding)

---
+++ MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+// O database context foi criado a partir do scaffolding.
+// Quando fazemos migrations para criar e mutar o banco de dados,
+// essas migrations são geradas a partir de nosso dbContext.
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)] 
+        // Utilizamos DataType.Date para que apenas a data seja necessária para inserção,
+        // e que só ela seja mostrada, retirando informações de horário, etc.
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
